ad1f71954b00244c7feb49f2d3dbd4672d4672c9
pandesantos <spd12@hotmail.com>
2017-02-07 22:05:33 +0545

    added plans and team section in home page

---
+++ resources/views/templates/plan.blade.php
@@ -0,0 +1,80 @@
+<section id="plan" class="bg-light-gray">
+    <div class="container">
+    <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="section-heading">Choose a Plan</h2>
+                    <h3 class="section-subheading text-muted"></h3>
+                </div>
+            </div>
+    <div class="row">
+                <div class="col-xs-6 col-sm-6 col-md-3 col-lg-4">
+                
+                    <!-- PRICE ITEM -->
+                    <div class="panel price panel-red">
+                        <div class="panel-heading  text-center">
+                        <h3><u>EXCLUSIVE</u></h3>
+                        </div>
+                        <div class="panel-body text-center">
+                            <p class="lead" style="font-size:40px"><strong>Rs 10,000 </strong></p>
+                        </div>
+                        <ul class="list-group list-group-flush text-center">
+                            <li class="list-group-item"><i class="icon-ok text-danger"></i> 10,000 View</li>
+                            <li class="list-group-item"><i class="icon-ok text-danger"></i> 5000 Click</li>
+                            <li class="list-group-item"><i class="icon-ok text-danger"></i> 24/7 support</li>
+                        </ul>
+                        <div class="panel-footer">
+                            <a class="btn btn-lg btn-block btn-danger" href="#">BUY NOW!</a>
+                        </div>
+                    </div>
+                    <!-- /PRICE ITEM -->
+                    
+                    
+                </div>
+                
+                <div class="col-xs-6 col-sm-6 col-md-3 col-lg-4">
+                
+                    <!-- PRICE ITEM -->
+                    <div class="panel price panel-blue">
+                        <div class="panel-heading arrow_box text-center">
+                        <h3><u>ENTERPRISE</u></h3>
+                        </div>
+                        <div class="panel-body text-center">
+                            <p class="lead" style="font-size:40px"><strong>Rs 5,000 </strong></p>
+                        </div>
+                        <ul class="list-group list-group-flush text-center">
+                            <li class="list-group-item"><i class="icon-ok text-info"></i> 5000 View</li>
+                            <li class="list-group-item"><i class="icon-ok text-info"></i> 2500 Click</li>
+                            <li class="list-group-item"><i class="icon-ok text-info"></i> 24/7 support</li>
+                        </ul>
+                        <div class="panel-footer">
+                            <a class="btn btn-lg btn-block btn-info" href="#">BUY NOW!</a>
+                        </div>
+                    </div>
+                    <!-- /PRICE ITEM -->
+                    
+                    
+                </div>
+                
+                <div class="col-xs-6 col-sm-6 col-md-3 col-lg-4">           
+                    <!-- PRICE ITEM -->
+                    <div class="panel price panel-green">
+                        <div class="panel-heading arrow_box text-center">
+                        <h3><u>NORMAL</u></h3>
+                        </div>
+                        <div class="panel-body text-center">
+                            <p class="lead" style="font-size:40px"><strong>Rs 1,000</strong></p>
+                        </div>
+                        <ul class="list-group list-group-flush text-center">
+                            <li class="list-group-item"><i class="icon-ok text-success"></i> 1000 View </li>
+                            <li class="list-group-item"><i class="icon-ok text-success"></i> 500 Click</li>
+                            <li class="list-group-item"><i class="icon-ok text-success"></i> 24/7 support</li>
+                        </ul>
+                        <div class="panel-footer">
+                            <a class="btn btn-lg btn-block btn-success" href="#">BUY NOW!</a>
+                        </div>
+                    </div>
+                    <!-- /PRICE ITEM -->      
+                 </div>         
+            </div>            
+</div>
+    </section>
---
+++ resources/views/templates/team.blade.php
@@ -0,0 +1,78 @@
+<section id="team">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="section-heading">Our Amazing Team</h2>
+                    <h3 class="section-subheading text-muted"></h3> 
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-3">
+                    <div class="team-member">
+                        <img src="img/team/1.jpg" class="img-responsive img-circle" alt="">
+                        <h4>Nischal Subedi</h4>
+                        <p class="text-muted">Front End Designer</p>
+                        <ul class="list-inline social-buttons">
+                            <li><a href="#"><i class="fa fa-twitter"></i></a>
+                            </li>
+                            <li><a href="#"><i class="fa fa-facebook"></i></a>
+                            </li>
+                            <li><a href="#"><i class="fa fa-linkedin"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="team-member">
+                        <img src="img/team/2.jpg" class="img-responsive img-circle" alt="">
+                        <h4>Prabin Kharel</h4>
+                        <p class="text-muted">Front End Designer</p>
+                        <ul class="list-inline social-buttons">
+                            <li><a href="#"><i class="fa fa-twitter"></i></a>
+                            </li>
+                            <li><a href="#"><i class="fa fa-facebook"></i></a>
+                            </li>
+                            <li><a href="#"><i class="fa fa-linkedin"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-sm-3">
+                    <div class="team-member">
+                        <img src="img/team/2.jpg" class="img-responsive img-circle" alt="">
+                        <h4>Santosh Pandey</h4>
+                        <p class="text-muted">Back End Developer</p>
+                        <ul class="list-inline social-buttons">
+                            <li><a href="#"><i class="fa fa-twitter"></i></a>
+                            </li>
+                            <li><a href="#"><i class="fa fa-facebook"></i></a>
+                            </li>
+                            <li><a href="#"><i class="fa fa-linkedin"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-sm-3">
+                    <div class="team-member">
+                        <img src="img/team/3.jpg" class="img-responsive img-circle" alt="">
+                        <h4>Sushil Pyakurel</h4>
+                        <p class="text-muted">Back End Developer</p>
+                        <ul class="list-inline social-buttons">
+                            <li><a href="#"><i class="fa fa-twitter"></i></a>
+                            </li>
+                            <li><a href="#"><i class="fa fa-facebook"></i></a>
+                            </li>
+                            <li><a href="#"><i class="fa fa-linkedin"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <p class="large text-muted"></p>
+                </div>
+            </div>
+        </div>
+    </section>
